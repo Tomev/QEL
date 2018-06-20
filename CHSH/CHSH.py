59db275bfb8a41ee2cd8e5e84e35fd17575d2080
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2018-06-20 18:11:32 +0200

    [STABLE] CHSH circuits creation placed in methods for future use.

--- CHSH/CHSH.py
+++ CHSH/CHSH.py
@@ -4,63 +4,69 @@ from methods import consts
 import time
 import numpy as np
 
+
+def get_chsh_circuits():
+    # Creating registers
+    q = QuantumRegister(2)
+    c = ClassicalRegister(2)
+
+    # quantum circuit to make an entangled bell state
+    bell = QuantumCircuit(q, c)
+    bell.h(q[0])
+    bell.cx(q[0], q[1])
+
+    # quantum circuit to measure q in the standard basis
+    measure_zz = QuantumCircuit(q, c)
+    measure_zz.measure(q[0], c[0])
+    measure_zz.measure(q[1], c[1])
+
+    # quantum circuit to measure q in the superposition basis
+    measure_xx = QuantumCircuit(q, c)
+    measure_xx.h(q[0])
+    measure_xx.h(q[1])
+    measure_xx.measure(q[0], c[0])
+    measure_xx.measure(q[1], c[1])
+
+    # quantum circuit to measure ZX
+    measure_zx = QuantumCircuit(q, c)
+    measure_zx.h(q[0])
+    measure_zx.measure(q[0], c[0])
+    measure_zx.measure(q[1], c[1])
+
+    # quantum circuit to measure XZ
+    measure_xz = QuantumCircuit(q, c)
+    measure_xz.h(q[1])
+    measure_xz.measure(q[0], c[0])
+    measure_xz.measure(q[1], c[1])
+
+    measure = [measure_zz, measure_zx, measure_xx, measure_xz]
+
+    real_chsh_circuits = []
+    real_x = []
+
+    real_steps = 10
+
+    for step in range(real_steps):
+
+        theta = 2.0 * np.pi * step / 10
+        bell_middle = QuantumCircuit(q, c)
+        bell_middle.ry(theta, q[0])
+
+        for m in measure:
+            real_chsh_circuits.append(bell + bell_middle + m)
+
+        real_x.append(theta)
+
+    return real_chsh_circuits
+
+
 # Prepare circuits here.
-
-# Creating registers
-q = QuantumRegister(2)
-c = ClassicalRegister(2)
-
-# quantum circuit to make an entangled bell state
-bell = QuantumCircuit(q, c)
-bell.h(q[0])
-bell.cx(q[0], q[1])
-
-# quantum circuit to measure q in the standard basis
-measureZZ = QuantumCircuit(q, c)
-measureZZ.measure(q[0], c[0])
-measureZZ.measure(q[1], c[1])
-
-# quantum circuit to measure q in the superposition basis
-measureXX = QuantumCircuit(q, c)
-measureXX.h(q[0])
-measureXX.h(q[1])
-measureXX.measure(q[0], c[0])
-measureXX.measure(q[1], c[1])
-
-# quantum circuit to measure ZX
-measureZX = QuantumCircuit(q, c)
-measureZX.h(q[0])
-measureZX.measure(q[0], c[0])
-measureZX.measure(q[1], c[1])
-
-# quantum circuit to measure XZ
-measureXZ = QuantumCircuit(q, c)
-measureXZ.h(q[1])
-measureXZ.measure(q[0], c[0])
-measureXZ.measure(q[1], c[1])
-
-measure = [measureZZ, measureZX, measureXX, measureXZ]
-
-real_chsh_circuits = []
-real_x = []
-
-real_steps = 10
-
-for step in range(real_steps):
-
-    theta = 2.0 * np.pi * step / 10
-    bell_middle = QuantumCircuit(q, c)
-    bell_middle.ry(theta, q[0])
-
-    for m in measure:
-        real_chsh_circuits.append(bell+bell_middle+m)
-
-    real_x.append(theta)
+chsh_circuits = get_chsh_circuits()
 
 print('Circuit prepared for execution.')
 
 # Assign circuits to run here.
-circuits = real_chsh_circuits
+circuits = chsh_circuits
 
 current_backend_index = 0
 
