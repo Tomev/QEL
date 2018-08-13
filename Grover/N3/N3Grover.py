e705adb0fec464cc8cd6a80ec3a2daf5e6377eb0
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2018-08-14 01:25:17 +0200

    [STABLE] Added qasms for other oracles to available files list.

--- Grover/N3/N3Grover.py
+++ Grover/N3/N3Grover.py
@@ -1,7 +1,8 @@
 import qiskit
 from methods import run_main_loop
 
-available_file_names = ['Grover_N3.qasm']
+available_file_names = ['Grover_N3_000.qasm', 'Grover_N3_001.qasm', 'Grover_N3_010.qasm', 'Grover_N3_011.qasm'
+                        'Grover_N3_100.qasm', 'Grover_N3_101.qasm', 'Grover_N3_110.qasm', 'Grover_N3_111.qasm']
 selected_file_index = 0
 circuit = qiskit.load_qasm_file(available_file_names[selected_file_index], 'GroverN3')
 
