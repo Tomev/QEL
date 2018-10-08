378b35385f88df4389fdde56eeddf90827198d76
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2018-10-08 17:42:55 +0200

    Added simplified version of N3 Grover 010.

--- Grover/N3/N3Grover.py
+++ Grover/N3/N3Grover.py
@@ -4,9 +4,9 @@ from methods import run_main_loop, test_locally
 #available_file_names = ['Grover_N3_000.qasm', 'Grover_N3_001.qasm', 'Grover_N3_010.qasm', 'Grover_N3_011.qasm',
 #                        'Grover_N3_100.qasm', 'Grover_N3_101.qasm', 'Grover_N3_110.qasm', 'Grover_N3_111.qasm']
 
-available_file_names = ['Grover_N3_000_simplified.qasm', 'Grover_N3_001_simplified.qasm']
+available_file_names = ['Grover_N3_000_simplified.qasm', 'Grover_N3_001_simplified.qasm', 'Grover_N3_010_simplified.qasm']
 
-selected_file_index = 0
+selected_file_index = 2
 circuit = qiskit.load_qasm_file(available_file_names[selected_file_index], 'GroverN3')
 
 #run_main_loop(circuit)
