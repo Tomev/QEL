c5bdee710765b7fdc7619de6c7d7da71b0f897e2
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2018-10-27 17:51:53 +0200

    [STABLE] GroverN3 now sends simplified and base version of selected state qasm.

--- Grover/N3/N3Grover.py
+++ Grover/N3/N3Grover.py
@@ -3,16 +3,22 @@ import sys
 sys.path.append('../..')
 from methods import run_main_loop, test_locally
 
-#available_file_names = ['Grover_N3_000.qasm', 'Grover_N3_001.qasm', 'Grover_N3_010.qasm', 'Grover_N3_011.qasm',
-#                        'Grover_N3_100.qasm', 'Grover_N3_101.qasm', 'Grover_N3_110.qasm', 'Grover_N3_111.qasm']
+available_file_names = ['Grover_N3_000.qasm', 'Grover_N3_001.qasm', 'Grover_N3_010.qasm', 'Grover_N3_011.qasm',
+                        'Grover_N3_100.qasm', 'Grover_N3_101.qasm', 'Grover_N3_110.qasm', 'Grover_N3_111.qasm']
 
-available_file_names = \
+available_simplified_file_names = \
     ['Grover_N3_000_simplified.qasm', 'Grover_N3_001_simplified.qasm', 'Grover_N3_010_simplified.qasm',
      'Grover_N3_011_simplified.qasm', 'Grover_N3_100_simplified.qasm', 'Grover_N3_101_simplified.qasm',
      'Grover_N3_110_simplified.qasm', 'Grover_N3_111_simplified.qasm']
 
-selected_file_index = 7
-circuit = qiskit.load_qasm_file(available_file_names[selected_file_index], available_file_names[selected_file_index])
+selected_file_index = 0
+
+circuits = []
+
+circuits.append(qiskit.load_qasm_file(available_file_names[selected_file_index],
+                                      available_file_names[selected_file_index]))
+circuits.append(qiskit.load_qasm_file(available_simplified_file_names[selected_file_index],
+                                      available_simplified_file_names[selected_file_index]))
 
 run_main_loop(circuit)
-#test_locally(circuit)
+test_locally(circuits)
