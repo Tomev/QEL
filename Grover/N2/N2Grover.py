6a52ed0f6c167aa7060d3f4ff11c9658cc0b0fde
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2018-12-16 22:29:27 +0100

    N2Grover now sends all states in one job. Qasms modified in such way that they use only 2 required qubits.

--- Grover/N2/N2Grover.py
+++ Grover/N2/N2Grover.py
@@ -1,12 +1,18 @@
+import sys
+sys.path.append('../..')
 from qiskit import load_qasm_file
-from methods import run_main_loop
+from methods import run_main_loop, test_locally
 
 available_file_names = ['Grover_N_2_A_00.qasm', 'Grover_N_2_A_01.qasm', 'Grover_N_2_A_10.qasm', 'Grover_N_2_A_11.qasm']
 selected_file_index = 0
 
-circuit = load_qasm_file(available_file_names[selected_file_index], 'GroverN2')
+circuits = []
 
-run_main_loop(circuit)
+for file_name in available_file_names:
+    circuits.append(load_qasm_file(file_name, file_name))
+
+#test_locally(circuits)
+run_main_loop(circuits)
 
 
 
