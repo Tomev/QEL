6abdf9ef63ab80a16195be9af58a3ca11f832d82
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2018-12-27 22:55:32 +0100

    Grover N2 prepared for running with CHSH tests.

--- Grover/N2/N2Grover.py
+++ Grover/N2/N2Grover.py
@@ -1,7 +1,6 @@
 import sys
 sys.path.append('../..')
-from qiskit import load_qasm_file
-from methods import run_main_loop, test_locally
+from methods import run_main_loop, test_locally, create_circuit_from_qasm, run_main_loop_with_chsh_test
 
 available_file_names = ['Grover_N_2_A_00.qasm', 'Grover_N_2_A_01.qasm', 'Grover_N_2_A_10.qasm', 'Grover_N_2_A_11.qasm']
 selected_file_index = 0
@@ -9,7 +8,10 @@ selected_file_index = 0
 circuits = []
 
 for file_name in available_file_names:
-    circuits.append(load_qasm_file(file_name, file_name))
+    qc = create_circuit_from_qasm(file_name)
+    qc.name = file_name
+    circuits.append(qc)
 
+run_main_loop_with_chsh_test(circuits)
 #test_locally(circuits)
-run_main_loop(circuits)
+#run_main_loop(circuits)
