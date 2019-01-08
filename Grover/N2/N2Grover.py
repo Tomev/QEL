46603b7f12c196a75cff0628e8f850c61d60ac0c
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2019-01-08 09:17:03 +0100

    Grover N2 updated to respect circuit naming convention.

--- Grover/N2/N2Grover.py
+++ Grover/N2/N2Grover.py
@@ -1,17 +1,16 @@
 import sys
 sys.path.append('../..')
-from methods import run_main_loop, test_locally, create_circuit_from_qasm, run_main_loop_with_chsh_test
+from methods import test_locally, create_circuit_from_qasm, run_main_loop_with_chsh_test
 
-available_file_names = ['Grover_N2_00.qasm', 'Grover_N2_01.qasm', 'Grover_N2_10.qasm', 'Grover_N2_11.qasm']
+available_file_names = ['Grover_2_00_1.qasm', 'Grover_2_01_1.qasm', 'Grover_2_10_1.qasm', 'Grover_2_11_1.qasm']
 selected_file_index = 0
 
 circuits = []
 
 for file_name in available_file_names:
     qc = create_circuit_from_qasm(file_name)
-    qc.name = file_name
+    qc.name = file_name.split('.')[0]
     circuits.append(qc)
 
-run_main_loop_with_chsh_test(circuits)
-#test_locally(circuits)
-#run_main_loop(circuits)
+#run_main_loop_with_chsh_test(circuits)
+test_locally(circuits)
