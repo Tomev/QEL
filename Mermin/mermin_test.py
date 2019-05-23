2fca21051541649fa562d9e70a13ad2a44a183a3
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2019-05-23 21:49:38 +0200

    Added barrier circuits to Mermin script.

--- Mermin/mermin_test.py
+++ Mermin/mermin_test.py
@@ -9,10 +9,17 @@ def get_mermin_circuits():
     # quantum circuit to make GHZ state 
     q3 = QuantumRegister(3)
     c3 = ClassicalRegister(3)
+
     ghz3 = QuantumCircuit(q3, c3)
     ghz3.h(q3[0])
     ghz3.cx(q3[0], q3[1])
     ghz3.cx(q3[0], q3[2])
+
+    bghz3 = QuantumCircuit(q3, c3)
+    bghz3.h(q3[0])
+    bghz3.cx(q3[0], q3[1])
+    bghz3.cx(q3[0], q3[2])
+    bghz3.barrier()
 
     # quantum circuit to measure XXX
     measure_xxx = QuantumCircuit(q3, c3)
@@ -22,8 +29,12 @@ def get_mermin_circuits():
     measure_xxx.measure(q3[0], c3[0])
     measure_xxx.measure(q3[1], c3[1])
     measure_xxx.measure(q3[2], c3[2])
+
     ghz_xxx = ghz3 + measure_xxx
     ghz_xxx.name = "Mermin_XXX"
+
+    bghz_xxx = bghz3 + measure_xxx
+    bghz_xxx.name = "B_Mermin_XXX"
 
     # Quantum circuit to measure XYY
     measure_xyy = QuantumCircuit(q3, c3)
@@ -35,8 +46,12 @@ def get_mermin_circuits():
     measure_xyy.measure(q3[0], c3[0])
     measure_xyy.measure(q3[1], c3[1])
     measure_xyy.measure(q3[2], c3[2])
+
     ghz_xyy = ghz3 + measure_xyy
     ghz_xyy.name = "Mermin_XYY"
+
+    bghz_xyy = bghz3 + measure_xyy
+    bghz_xyy.name = "B_Mermin_XYY"
 
     # quantum circuit to measure q YXY
     measure_yxy = QuantumCircuit(q3, c3)
@@ -48,8 +63,12 @@ def get_mermin_circuits():
     measure_yxy.measure(q3[0], c3[0])
     measure_yxy.measure(q3[1], c3[1])
     measure_yxy.measure(q3[2], c3[2])
+
     ghz_yxy = ghz3 + measure_yxy
     ghz_yxy.name = "Mermin_YXY"
+
+    bghz_yxy = bghz3 + measure_yxy
+    bghz_yxy.name = "B_Mermin_YXY"
 
     # quantum circuit to measure q YYX
     measure_yyx = QuantumCircuit(q3, c3)
@@ -61,14 +80,18 @@ def get_mermin_circuits():
     measure_yyx.measure(q3[0], c3[0])
     measure_yyx.measure(q3[1], c3[1])
     measure_yyx.measure(q3[2], c3[2])
+
     ghz_yyx = ghz3 + measure_yyx
     ghz_yyx.name = "Mermin_YYX"
 
-    circuits = [ghz_xxx, ghz_yyx, ghz_yxy, ghz_xyy]
+    bghz_yyx = bghz3 + measure_yyx
+    bghz_yyx.name = "B_Mermin_YYX"
+
+    circuits = [ghz_xxx, ghz_yyx, ghz_yxy, ghz_xyy, bghz_xxx, bghz_yyx, bghz_yxy, bghz_xyy, ]
 
     return circuits
 
 
-# run_main_loop_with_chsh_test(get_mermin_circuits())
-test_locally(get_mermin_circuits())
+run_main_loop_with_chsh_test(get_mermin_circuits())
+# test_locally(get_mermin_circuits())
 
