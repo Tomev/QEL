6a2ddfba3a0df0169236fdc0f76c44289fab8092
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2019-06-06 20:12:17 +0200

    Fixed error in backend monitor.

--- Tools/backend_monitor.py
+++ Tools/backend_monitor.py
@@ -1,7 +1,8 @@
+import sys
+sys.path.append('..\\')
+import methods
 from qiskit import IBMQ
 from qiskit.tools.monitor import backend_monitor
-
-IBMQ.load_accounts()
 
 backend = IBMQ.get_backend('ibmqx2')
 backend_monitor(backend)
