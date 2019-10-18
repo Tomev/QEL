3b952c46ff44823ec80d0c3f23b629cc46efe001
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2019-10-18 07:14:40 +0200

    Sanity check measurements fix.

--- SanityCheck/SanityCheck.py
+++ SanityCheck/SanityCheck.py
@@ -13,34 +13,28 @@ cr3 = ClassicalRegister(3)
 bases_N2 = ['ZZ', 'ZX', 'XX', 'XZ']
 bases_N3 = {'XXX', 'YYX', 'YXY', 'XYY'}
 
-
 c1 = QuantumCircuit(qr2, cr2)
 c1.name = "SC_00"
-c1.measure(qr2, cr2)
 
 c2 = QuantumCircuit(qr2, cr2)
 c2.x(qr2[0])
 c2.x(qr2[1])
 c2.name = "SC_11"
-c2.measure(qr2, cr2)
 
 c3 = QuantumCircuit(qr2, cr2)
 c3.x(qr2[0])
 c3.x(qr2[1])
 c3.barrier()
 c3.name = "SC_11_B"
-c3.measure(qr2, cr2)
 
 c4 = QuantumCircuit(qr3, cr3)
 c4.name = "SC_000"
-c4.measure(qr3, cr3)
 
 c5 = QuantumCircuit(qr3, cr3)
 c5.x(qr3[0])
 c5.x(qr3[1])
 c5.x(qr3[2])
 c5.name = "SC_111"
-c5.measure(qr3, cr3)
 
 c6 = QuantumCircuit(qr3, cr3)
 c6.x(qr3[0])
@@ -48,7 +42,7 @@ c6.x(qr3[1])
 c6.x(qr3[2])
 c6.barrier()
 c6.name = "SC_111_B"
-c6.measure(qr3, cr3)
+
 
 circuits_2d = [c1, c2, c3]
 circuits_3d = [c4, c5, c6]
