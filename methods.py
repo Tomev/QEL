6573a54f2051b759d034addc99bc1478eb3a81c5
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2019-10-24 08:46:03 +0200

    Main loop works well after IBMQuantumExperience shutdown. Main loop catches the NO_CREDIT_EXCEPTIONS and waits after before next execution try.

--- methods.py
+++ methods.py
@@ -1,5 +1,6 @@
 from qiskit import execute, IBMQ, Aer, QuantumCircuit, QuantumRegister, ClassicalRegister, exceptions
 from qiskit.providers.aer import noise
+from qiskit.providers.ibmq.job.ibmqjob import IBMQJob, JobStatus
 from IBMQuantumExperience import IBMQuantumExperience
 import numpy as np
 import time
@@ -33,7 +34,10 @@ def get_sim_backend_from_name(name):
     return Aer.get_backend(name)
 
 
+# Legacy
 def get_current_credits():
+    # return 3  # placeholder, until I get answer from IBM
+    # This method will cause errors, as IBMQuantumExperience server is now shut down.
     api = IBMQuantumExperience(token=Qconfig.APItoken, config=Qconfig.config)
     return api.get_my_credits()['remaining']
 
@@ -51,12 +55,12 @@ def execute_circuits(circuits, backend, use_mapping=False, noise_model=None):
         print("Used mapping:")
         print(mapping)
         return execute(circuits, backend, shots=consts.SHOTS, coupling_map=mapping)
-    elif noise_model != None:
-      real_chip = get_backend_from_name(consts.CONSIDERED_REMOTE_BACKENDS[0])
-      mapping = real_chip.configuration().coupling_map
-      basis_gates = noise_model.basis_gates
-      return execute(circuits, backend, noise_model=noise_model, shots=consts.SHOTS,
-                     basis_gates=basis_gates, coupling_map=mapping)
+    elif noise_model is not None:
+        real_chip = get_backend_from_name(consts.CONSIDERED_REMOTE_BACKENDS[0])
+        mapping = real_chip.configuration().coupling_map
+        basis_gates = noise_model.basis_gates
+        return execute(circuits, backend, noise_model=noise_model, shots=consts.SHOTS,
+                       basis_gates=basis_gates, coupling_map=mapping)
     else:
         return execute(circuits, backend, shots=consts.SHOTS)
 
@@ -64,7 +68,6 @@ def execute_circuits(circuits, backend, use_mapping=False, noise_model=None):
 def run_main_loop(circuits):
     current_backend_index = 0
     wait_time_in_minutes = 5
-    credits_required = 3
 
     if not os.path.isfile(os.path.join(os.path.dirname(__file__), 'current_iteration_holder.txt')):
         file = open(os.path.join(os.path.dirname(__file__), 'current_iteration_holder.txt'), "a")
@@ -76,18 +79,9 @@ def run_main_loop(circuits):
     iterations_done = int(line)
     file.close()
 
-    for iteration_number in range(consts.ITERATIONS_NUMBER - iterations_done):
+    while iterations_done < consts.ITERATIONS_NUMBER:
 
-        print('Iteration number: ', iteration_number + iterations_done)
-
-        current_credits_number = get_current_credits()
-
-        # In case there are to little credits
-        while current_credits_number < credits_required:
-            print("Current credits number is", current_credits_number,
-                  'which is less than 3. Waiting ' + str(wait_time_in_minutes) + ' minute(s) to continue.')
-            time.sleep(wait_time_in_minutes * 60)
-            current_credits_number = get_current_credits()
+        print(f'Iteration number: { iterations_done}')
 
         print('Getting available backends...')
         operational_remote_backends = get_operational_remote_backends()
@@ -112,15 +106,23 @@ def run_main_loop(circuits):
 
             print("Executing quantum program on %s." % backend_name)
 
-            execute_circuits(circuits, get_backend_from_name(backend_name))
+            job = execute_circuits(circuits, get_backend_from_name(backend_name))
+
+            while job.status() == JobStatus.INITIALIZING:
+                print(job.status())
+                time.sleep(10)
 
             print("Program sent for execution to ", backend_name, '.')
             current_backend_index = (current_backend_index + 1) % len(consts.CONSIDERED_REMOTE_BACKENDS)
 
-        except exceptions as ex:
+        except BaseException as ex:
             print('There was an error in the circuit!. Error = {}'.format(ex))
+            print(f'Waiting {wait_time_in_minutes} before next try.')
+            time.sleep(wait_time_in_minutes * 60)
+            continue
 
-        line = str(iteration_number + iterations_done + 1)
+        iterations_done += 1
+        line = str(iterations_done)
         print(f'Writing to file: {line}')
         file = open(os.path.join(os.path.dirname(__file__), 'current_iteration_holder.txt'), 'w')
         file.write(line)
@@ -178,7 +180,7 @@ def test_locally_with_noise(circuits, save_to_file=False, number_of_simulations=
         for i in range(number_of_simulations):
             executed_job = execute_circuits(circuits, backend, noise_model=noise_model)
             simulation_report_content += parse_job_to_report_string(executed_job)
-            print(f'Simulation {i+1} done.')
+            print(f'Simulation {i + 1} done.')
 
         # Save gathered data to file.
         file = open("sim_report.csv", "w")
@@ -202,13 +204,13 @@ def get_jobs_from_backend(backend_name, jobs_number=consts.JOBS_DOWNLOAD_LIMIT):
     download_number = min(max_download_number, consts.MAX_JOBS_SINGLE_DOWNLOAD_NUM)
 
     while number_of_jobs_to_download > 0:
-
         print("Number of jobs to download left: %i." % number_of_jobs_to_download)
 
         number_of_jobs_to_download_now = min(download_number, number_of_jobs_to_download)
 
         number_of_jobs_to_download -= number_of_jobs_to_download_now
-        downloaded_jobs.extend(backend.jobs(limit=number_of_jobs_to_download_now, skip=number_of_jobs_to_skip, status='DONE'))
+        downloaded_jobs.extend(
+            backend.jobs(limit=number_of_jobs_to_download_now, skip=number_of_jobs_to_skip, status='DONE'))
         number_of_jobs_to_skip += number_of_jobs_to_download_now
 
     return downloaded_jobs
@@ -237,33 +239,32 @@ def parse_job_to_report_string(job):
 
 
 def report_to_csv(csv_file, report_file=consts.JOBS_FILE_NAME, sep=consts.CSV_SEPARATOR, lowercase_header=True):
-
     data_file = pd.read_csv(report_file, sep=sep)
-    
+
     # Evaling string representations of dictionaries in 'Results' column ("{"00":500, "11":524}")
     results = data_file.Results
     results = [eval(r) for r in results]
-    
+
     # Creating a data frame with results in the long format (1, "00", 500 // 1, "11", 524)
     # Auxiliary column "row_num" contains row index from the original data frame - to enable joining
     # Column "variable" contains keys from the dictionaries (names of states)
     # Column "value" contains their respective values (count)
     results = pd.DataFrame([[i, str(r), results[i][r]] for i in range(len(results)) for r in results[i]],
-                            columns=['row_num', 'variable', 'value'])
-    
+                           columns=['row_num', 'variable', 'value'])
+
     # Joining results with other experimental data
-    df_long = data_file.merge(results, left_index=True, right_on = 'row_num').drop(['row_num', 'Results'], 1)
-    
+    df_long = data_file.merge(results, left_index=True, right_on='row_num').drop(['row_num', 'Results'], 1)
+
     # Converting uppercase in header names to lowercase
     if lowercase_header:
         df_long.columns = [c.lower() for c in df_long.columns]
-    
-    df_long.to_csv(csv_file, index=False)    
+
+    df_long.to_csv(csv_file, index=False)
 
 
 def add_measure_in_base(qc: QuantumCircuit, base: str):
-    #qr = QuantumRegister(len(base))
-    #cr = ClassicalRegister(len(base))
+    # qr = QuantumRegister(len(base))
+    # cr = ClassicalRegister(len(base))
 
     base = base.upper()
 
@@ -292,12 +293,12 @@ def add_measure_in_base(qc: QuantumCircuit, base: str):
 def get_chsh_circuits():
     q = QuantumRegister(2)
     c = ClassicalRegister(2)
-    
+
     # Circuit to prepare an entangled state.
     bell = QuantumCircuit(q, c)
     bell.h(q[0])
     bell.cx(q[0], q[1])
-    bell.ry(np.pi/4, q[0])
+    bell.ry(np.pi / 4, q[0])
 
     # Circuits to measure q to c in different basis.
     bases = ['ZZ', 'ZX', 'XX', 'XZ']
@@ -311,7 +312,7 @@ def get_chsh_circuits():
     chsh_circuits[1].name = 'CHSH-test_ZX'
     chsh_circuits[2].name = 'CHSH-test_XX'
     chsh_circuits[3].name = 'CHSH-test_XZ'
-    
+
     return chsh_circuits
 
 
@@ -338,34 +339,34 @@ def custom_backend_monitor(backend):
     qubit_header = 'Qubits [Name / Freq / T1 / T2 / U1 err / U2 err / U3 err / Readout err]'
 
     backend_info = backend_info + qubit_header + '\n'
-    backend_info = backend_info + '-'*len(qubit_header) + '\n'
+    backend_info = backend_info + '-' * len(qubit_header) + '\n'
 
     for qub in range(len(props['qubits'])):
         name = 'Q%s' % qub
         qubit_data = props['qubits'][qub]
-        gate_data = props['gates'][3*qub:3*qub+3]
+        gate_data = props['gates'][3 * qub:3 * qub + 3]
         t1_info = qubit_data[0]
         t2_info = qubit_data[1]
         freq_info = qubit_data[2]
         readout_info = qubit_data[3]
 
-        freq = str(round(freq_info['value'], 5))+' '+freq_info['unit']
-        T1 = str(round(t1_info['value'], 5))+' ' + t1_info['unit']
-        T2 = str(round(t2_info['value'], 5))+' ' + t2_info['unit']
+        freq = str(round(freq_info['value'], 5)) + ' ' + freq_info['unit']
+        T1 = str(round(t1_info['value'], 5)) + ' ' + t1_info['unit']
+        T2 = str(round(t2_info['value'], 5)) + ' ' + t2_info['unit']
         U1 = str(round(gate_data[0]['parameters'][0]['value'], 5))
         U2 = str(round(gate_data[1]['parameters'][0]['value'], 5))
         U3 = str(round(gate_data[2]['parameters'][0]['value'], 5))
         readout_error = str(round(readout_info['value'], 5))
         qstr = sep.join([name, freq, T1, T2, U1, U2, U3, readout_error])
 
-        backend_info = backend_info + offset+qstr + '\n'
+        backend_info = backend_info + offset + qstr + '\n'
 
     backend_info = backend_info + '\n'
-    multi_qubit_gates = props['gates'][3*config['n_qubits']:]
+    multi_qubit_gates = props['gates'][3 * config['n_qubits']:]
     multi_header = 'Multi-Qubit Gates [Name / Type / Gate Error]'
 
     backend_info = backend_info + multi_header + '\n'
-    backend_info = backend_info + '-'*len(multi_header) + '\n'
+    backend_info = backend_info + '-' * len(multi_header) + '\n'
 
     for gate in multi_qubit_gates:
         name = gate['name']
@@ -379,7 +380,6 @@ def custom_backend_monitor(backend):
 
 # Legacy
 def save_calibration_data(backend_name, data):
-
     now = datetime.datetime.now()
     file_name = backend_name + "_" + now.strftime("%Y-%m-%d_%H-%M") + ".txt"
 
@@ -392,3 +392,4 @@ def save_calibration_data(backend_name, data):
 
 IBMQ.save_account(Qconfig.APItoken, overwrite=True)
 acc = IBMQ.load_account()
+acc.credentials
