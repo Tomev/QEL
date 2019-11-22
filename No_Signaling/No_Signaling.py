3fb5d9f05e401fdca677f4a7754fcd36463314cd
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2019-11-22 08:02:50 +0100

    Default CHSH test added to the loop.

--- No_Signaling/No_Signaling.py
+++ No_Signaling/No_Signaling.py
@@ -3,8 +3,9 @@ sys.path.append('..')
 from CHSH.CHSH import get_chsh_circuits
 from SanityCheck.SanityCheck import get_sc_circuits
 from Mermin.mermin_test import get_mermin_circuits
-from methods import run_main_loop
+from methods import run_main_loop, run_main_loop_with_chsh_test
 
 circuits = get_sc_circuits() + get_chsh_circuits() + get_mermin_circuits()
 
-run_main_loop(circuits)
+#run_main_loop(circuits)
+run_main_loop_with_chsh_test(circuits)
