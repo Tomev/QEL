994f1e115e91b1ef65d52b77a9dcf10dc7cb5c30
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2019-11-22 15:30:03 +0100

    Circuit drawer script added.

---
+++ Visualisation/Circuits/Circuits_Drawer.py
@@ -0,0 +1,14 @@
+import sys
+sys.path.append('../..')
+from CHSH.CHSH import get_chsh_circuits
+from Mermin.mermin_test import get_mermin_circuits
+
+experiments_circuits = get_chsh_circuits()
+#experiments_circuits = get_mermin_circuits()
+
+circuit_to_draw = experiments_circuits[0]
+
+plot = circuit_to_draw.draw(output='mpl')
+plot.show()
+
+print("Done")
