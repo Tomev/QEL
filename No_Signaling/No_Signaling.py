856005f4c47719e41266b8c1e7281bfec75f9be3
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2019-11-28 09:16:44 +0100

    Using run_main_loop w/o CHSH test in No_Signaling Circuit.

--- No_Signaling/No_Signaling.py
+++ No_Signaling/No_Signaling.py
@@ -3,10 +3,12 @@ sys.path.append('..')
 from CHSH.CHSH import get_chsh_circuits
 from SanityCheck.SanityCheck import get_sc_circuits
 from Mermin.mermin_test import get_mermin_circuits
-from methods import run_main_loop, run_main_loop_with_chsh_test, test_locally
+from methods import run_main_loop, run_main_loop_with_chsh_test, test_locally, test_locally_with_noise
 
 circuits = get_sc_circuits() + get_chsh_circuits() + get_mermin_circuits()
 
-#run_main_loop(circuits)
-run_main_loop_with_chsh_test(circuits)
-#test_locally(circuits, False, True, 1)
+print(len(circuits))
+
+run_main_loop(circuits)
+#run_main_loop_with_chsh_test(circuits)
+#test_locally(circuits, True, False, 1)
