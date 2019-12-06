af3f6ebdd1c5f14396909b9b0a2915ccb8006957
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2019-12-06 13:09:19 +0100

    No signaling experiment adjusted to experiments per job cap (75).

--- No_Signaling/No_Signaling.py
+++ No_Signaling/No_Signaling.py
@@ -5,10 +5,11 @@ from SanityCheck.SanityCheck import get_sc_circuits
 from Mermin.mermin_test import get_mermin_circuits
 from methods import run_main_loop, run_main_loop_with_chsh_test, test_locally, test_locally_with_noise
 
-circuits = get_sc_circuits() + get_chsh_circuits() + get_mermin_circuits()
+circuits = get_sc_circuits() + get_mermin_circuits() + get_chsh_circuits(5)
 
 print(len(circuits))
 
-run_main_loop(circuits)
+
+# run_main_loop(circuits)
 #run_main_loop_with_chsh_test(circuits)
 #test_locally(circuits, True, False, 1)
