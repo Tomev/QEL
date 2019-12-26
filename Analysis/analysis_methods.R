eec082b04834f8d7afbbbde59d0e43c94cae66cc
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2019-12-26 20:18:11 +0100

    Analysis scripts update.

--- Analysis/analysis_methods.R
+++ Analysis/analysis_methods.R
@@ -30,7 +30,7 @@ extract_circuit_data <- function(data, ..., pattern = '_', circuit_column = 'cir
   circuit_data = str_split_fixed(
     data[[circuit_column]],
     pattern,
-    max(var_positions)
+    max(var_positions)+1
     )
   
   #Create new columns (names of parameters)
@@ -41,7 +41,7 @@ extract_circuit_data <- function(data, ..., pattern = '_', circuit_column = 'cir
   return(data)
 }
 
-chsh_test <- function(data, print = TRUE){
+chsh_test <- function(data, ..., print = TRUE, filter = TRUE){
   
   "
   Process chsh-test data
@@ -58,10 +58,12 @@ chsh_test <- function(data, print = TRUE){
   The label good=TRUE is assigned to jobs with chsh>2.
   
   Output: the data frame with filtered out test circuits and new columns 'chsh' and 'good'.
+  If `filter` only the good jobs are left.
   
   If `print` prints how many jobs were selected
   
   TO DO: general column names, threshold value, more general tests?
+  TO DO: Plot
   "
   
   #Choose the data from chsh test
@@ -88,25 +90,33 @@ chsh_test <- function(data, print = TRUE){
     mutate(good = (abs(chsh)>2))
   
   if(print){
-    str_c('Jobs selected: ', sum(test_data$good), ' out of ', nrow(test_data))
+    str_c(
+      'CHSH test. Jobs selected: ',
+      sum(test_data$good),
+      ' out of ',
+      nrow(test_data)
+      ) %>% print
   }
   
   #Return the filtered data frame with test data joined using job id
-  left_join(
-    data %>%
-      filter(str_split_fixed(circuit,'_',2)[,1] != 'CHSH-test'),
-    test_data,
-    'id'
-  )
+  data %<>% filter(str_split_fixed(circuit,'_',2)[,1] != 'CHSH-test') %>%
+  left_join(test_data,'id')
+  
+  if(filter){data %<>% filter(good)}
+  
+  return(data)
 }
 
 process_chsh <- function(data, ..., barrier = F){
   #Extract the circuit data for usual chsh naming convention
   
-  data %<>% extract_circuit_data(theta0 = 2, observable = 3)
+  data %<>% extract_circuit_data(experiment = 1, theta0 = 2, observable = 3)
+  data %<>% filter(experiment == 'CHSH')
+  
   if(barrier){
     data %<>% extract_circuit_data(barrier = 4)
-    data$barrier = (data$barrier == 'B')
+    data %<>% mutate(barrier = ifelse(barrier == 'B', 'With', 'Without'))
+    data %<>% group_by(barrier)
   }
   
   #Numeric theta from string
@@ -125,43 +135,50 @@ process_chsh <- function(data, ..., barrier = F){
     sign = (-1)^((observable == 'XZ')+(variable %in% c ('01','10')))
   )
   
+  data %<>% group_by(theta, add = TRUE)
   
   return(data)
 }
 
 process_mermin <- function(data, ..., barrier = F){
   #Extract the circuit data for usual mermin naming convention
+  #TODO: processing barrier repeated
   
-  data %<>% extract_circuit_data(observable = 2)
-  if(barrier){
-    data %<>% extract_circuit_data(barrier = 3)
-    data$barrier = (data$barrier == 'B')
-  }
+  data %<>% extract_circuit_data(experiment = 1, observable = 2)
+  data %<>% filter(experiment == 'Mermin')
+  
   data %<>% mutate(
     sign = (-1)^((observable == 'XXX')+(variable %in% c('000','011','101','110')))
   )
   
+  if(barrier){
+    data %<>% extract_circuit_data(barrier = 3)
+    data %<>% mutate(barrier = ifelse(barrier == 'B', 'With', 'Without'))
+    data %<>% group_by(barrier)
+  }
+  
   return(data)
 }
 
-agg_data <- function(data){
-  #For bell-type experiments
+agg_data <- function(data, print_kable = FALSE){
   #Calculates the expected value and its error from all the jobs
-  data %>%
+  #For bell-type experiments
+  table = data  %>%
     group_by(observable, add = T) %>%
     summarise(total = sum(value), value = sum(sign*value)/total,
               var = (1+value)*(1-value)/total) %>%
     summarise(
       value = sum(value), dv = sqrt(sum(var))
     )
-}
-summary_table <- function(data, ...){
-  data %>%
-    mutate(
-      result = sprintf('%.4f +/- %.4f', value, dv)
-    ) %>%
-    select(..., result) %>%
-    knitr::kable()
+  if(print_kable){
+    table %<>%
+      mutate(
+        result = sprintf('%.4f +/- %.4f', value, dv)
+      ) %>%
+      select(-value, -dv) %>%
+      knitr::kable()
+  }
+  return(table)
 }
 
 pi_axis <- function(){
@@ -172,28 +189,36 @@ pi_axis <- function(){
 }
 
 chsh_plot <- function(data){
-  #Fit and plot a sinusoid
-  
-  #TO DO: different coefficients for grouped data
-  
+  #Fit and plot a sinusoid (different fits for each group if data is grouped)
   
   data %<>%
     mutate(chsh = value, delta_chsh = dv, chsh_theory = 2*(cos(theta)+sin(theta)))
   
   #Fit
-  eta = lm(chsh ~ 0+chsh_theory, data = data) %>% coef
+  eta_df = data %>% do(eta = lm(chsh ~ 0+chsh_theory, data = .) %>% coef) %>%
+    mutate(eta = eta[[1]])
   
   #Plot
-  ggplot(data, aes(x = theta, y = chsh))+
+  p = ggplot(data, aes(x = theta, y = chsh))+
     geom_errorbar(aes(ymin = chsh - delta_chsh, ymax = chsh + delta_chsh),
                   width = 0.03)+
     geom_point(size = 0.05, alpha = 0.5)+
-    annotate('text', label = str_c('eta = ', round(eta,3)),
-             x = min(data$theta)+0.2, y = 0)+
+    #annotate('text', label = str_c('eta = ', round(eta,3)),
+    #         x = min(data$theta)+0.2, y = 0)+
     geom_hline(yintercept = c(2,-2),lty=2)+
     stat_function(fun=function(x) 2*(cos(x)+sin(x)))+
-    stat_function(fun=function(x) 2*eta*(cos(x)+sin(x)))+
+    geom_line(data = eta_df %>%
+                mutate(buf = 0) %>%
+                left_join(
+                  tibble(
+                    buf = 0,
+                    theta = seq(min(data$theta), max(data$theta), 0.05))
+                  ) %>%
+                mutate(chsh = 2*eta*(cos(theta)+sin(theta))))+
     pi_axis()
+  
+  print(p)
+  return(eta_df %>% knitr::kable())
   }
 
 process_signaling <- function(data){
@@ -222,14 +247,13 @@ process_signaling <- function(data){
 }
 
 
-plot_signaling <- function(data,
+plot_signaling <- function(data, ...,
                            x = quo(index),
-                           y = quo(value),
                            error = quo(dv),
                            facet_x = NULL,
                            facet_y = NULL){
   
-  #TODO: align plots, enable adding custom layers
+  #TODO: align plots
   
   data %<>% group_by(!!x)
   if(!is.null(facet_x)){
@@ -243,12 +267,12 @@ plot_signaling <- function(data,
   p1 = ggplot(
     data %>%
       mutate(other_base = other_base %>% str_replace('_','')),
-    aes_(x=x, y = y, colour = quo(other_base))
+    aes_(x=x, y = quo(value), colour = quo(other_base))
   )+
     geom_point()+
-    geom_errorbar(aes(ymin = !!y-!!error, ymax = !!y+!!error), width = 0.1)+
-    facet_grid(rows = facet_x, cols = facet_y)
-    #labs(x = 'Observable', y = 'Expected value')
+    geom_errorbar(aes(ymin = value-!!error, ymax = value+!!error), width = 0.1)+
+    facet_grid(rows = vars(!!facet_x), cols = vars(!!facet_y), labeller = label_both)
+    labs(y = 'Expected value')
   
   #The delta/sigma bars
   p2 = ggplot(
@@ -258,11 +282,10 @@ plot_signaling <- function(data,
         sigma = sqrt(sum(dv^2)),
         q = delta/sigma)
   )+
-    geom_col(aes_string(x = x, y = 'q'))+
-    #labs(x = 'Observable', y = expression(Delta/sigma))+
+    geom_col(aes_(x = x, y = quo(q)))+
+    labs(y = expression(Delta/sigma))+
     geom_hline(yintercept = 3, lty = 2)+
-    facet_grid(rows = facet_x, cols = facet_y)
+    facet_grid(rows = vars(!!facet_x), cols = vars(!!facet_y), labeller = label_both)
   
-  print(p1)
-  print(p2)
+  return(list(p1,p2))
 }
