55f5a0cf57c51c8a0282c2aa60c0fe08921f4b42
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2020-02-06 21:49:17 +0100

    Calibration scripts for n3 and n2 added.

--- SanityCheck/SanityCheck.py
+++ SanityCheck/SanityCheck.py
@@ -1,3 +1,5 @@
+from typing import List
+
 import numpy as np
 import sys
 
@@ -5,6 +7,103 @@ sys.path.append('..')
 from methods import test_locally, run_main_loop, test_locally_with_noise, run_main_loop_with_chsh_test, \
     add_measure_in_base
 from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister
+
+
+def get_n2_calibration_circuits():
+    qr2 = QuantumRegister(2)
+    cr2 = ClassicalRegister(2)
+
+    calibration_base = 'XX'
+
+    # 00 State
+    c1 = QuantumCircuit(qr2, cr2)
+    c1.name = "Calibration_00"
+
+    # 01 B State
+    c3 = QuantumCircuit(qr2, cr2)
+    c3.x(qr2[0])
+    c3.barrier()
+    c3.name = "Calibration_01"
+
+    # 10 B State
+    c4 = QuantumCircuit(qr2, cr2)
+    c4.x(qr2[1])
+    c4.barrier()
+    c4.name = "Calibration_10"
+
+    # 11 B State
+    c2 = QuantumCircuit(qr2, cr2)
+    c2.x(qr2[0])
+    c2.x(qr2[1])
+    c2.barrier()
+    c2.name = "Calibration_11"
+
+    circuits_n2 = [c1, c3, c4, c2]
+    calibration_circuits = []
+
+    for c in circuits_n2:
+        calibration_circuits.append(add_measure_in_base(c.copy(), calibration_base))
+
+    return calibration_circuits
+
+
+def get_n3_calibration_circuits():
+    qr3 = QuantumRegister(3)
+    cr3 = ClassicalRegister(3)
+
+    calibration_base = 'XXX'
+
+    c0 = QuantumCircuit(qr3, cr3)
+    c0.name = "Calibration_000"
+
+    c1 = QuantumCircuit(qr3, cr3)
+    c1.x(qr3[0])
+    c1.barrier()
+    c1.name = "Calibration_001"
+
+    c2 = QuantumCircuit(qr3, cr3)
+    c2.x(qr3[1])
+    c2.barrier()
+    c2.name = "Calibration_010"
+
+    c3 = QuantumCircuit(qr3, cr3)
+    c3.x(qr3[0])
+    c3.x(qr3[1])
+    c3.barrier()
+    c3.name = "Calibration_011"
+
+    c4 = QuantumCircuit(qr3, cr3)
+    c4.x(qr3[2])
+    c4.barrier()
+    c4.name = "Calibration_100"
+
+    c5 = QuantumCircuit(qr3, cr3)
+    c5.x(qr3[0])
+    c5.x(qr3[2])
+    c5.barrier()
+    c5.name = "Calibration_101"
+
+    c6 = QuantumCircuit(qr3, cr3)
+    c6.x(qr3[1])
+    c6.x(qr3[2])
+    c6.barrier()
+    c6.name = "Calibration_110"
+
+    c7 = QuantumCircuit(qr3, cr3)
+    c7.x(qr3[0])
+    c7.x(qr3[1])
+    c7.x(qr3[2])
+    c7.barrier()
+    c7.name = "Calibration_111"
+
+    circuits_n3 = [c0, c1, c2, c3, c4, c5, c6, c7]
+
+    calibration_circuits = []
+
+    for c in circuits_n3:
+        calibration_circuits.append(add_measure_in_base(c.copy(), calibration_base))
+
+    return calibration_circuits
 
 
 def get_sc_n2_circuits():
--- Tools/aReportConverter.py
+++ Tools/aReportConverter.py
@@ -9,6 +9,7 @@ file_result_name = 'jobs_report.csv'
 # report_file = JOBS_FILE_NAME  # Default value
 
 report_file = 'raw_jobs_report.csv'
+
 report_to_csv(file_result_name, report_file=report_file)
 
 file_result_name = 'm_jobs_report.csv'
