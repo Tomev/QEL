0e096743b0fb0564ca6182a0062fd8cebef4ffd4
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2020-02-06 22:25:01 +0100

    Refactored jobs_clearer.

--- Tools/pending_jobs_clearer.py
+++ Tools/pending_jobs_clearer.py
@@ -1,3 +1,7 @@
+import sys
+
+sys.path.append('..')
+
 import consts
 from qiskit import IBMQ
 
@@ -5,17 +9,15 @@ backend = IBMQ.load_account().backends(consts.CONSIDERED_REMOTE_BACKENDS[0])[0]
 
 print(f'Clearing backend {backend}.')
 
-downloaded_jobs = []
-max_download_number = 10
-download_number = min(max_download_number, consts.MAX_JOBS_SINGLE_DOWNLOAD_NUM)
+jobs_to_cancel = []
 
-# This
-downloaded_jobs.extend(backend.jobs(limit=5, status='QUEUED'))
-downloaded_jobs.extend(backend.jobs(limit=5, status='RUNNING'))
+# This may need to include more statuses.
+jobs_to_cancel.extend(backend.jobs(limit=5, status='QUEUED'))
+jobs_to_cancel.extend(backend.jobs(limit=5, status='RUNNING'))
 
-print(f'Got {len(downloaded_jobs)} jobs.\n')
+print(f'Got {len(jobs_to_cancel)} jobs.\n')
 
-for j in downloaded_jobs:
+for j in jobs_to_cancel:
     j.cancel()
     print('CANCELLED')
 
