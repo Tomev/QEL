b150584ea2b40c6954e0058d611d490fb4830043
Wojciech Ciszewski <wojtrz13@gmail.com>
2020-03-04 21:58:33 +0100

    Refactor of the script for running fidelity circuits

--- Fourier/fidelity/run_fidelity_circuits.py
+++ Fourier/fidelity/run_fidelity_circuits.py
@@ -4,34 +4,48 @@ from qiskit import IBMQ, QuantumCircuit
 from methods import run_main_loop, test_locally, test_locally_with_noise
 
 
-def main():
-    fourier_circuit = QuantumCircuit.from_qasm_file('../circuits/F3X.txt')
-    fourier_circuit.data = fourier_circuit.data[:-3]
-    inverted_fourier_circuit = fourier_circuit.inverse()
-    fourier_circuit.barrier()
-    fidelity_circuit = fourier_circuit.extend(inverted_fourier_circuit)
-    fidelity_circuit.measure(range(3), range(3))
-
-    circuits = []
-
-    for state in range(8):
-        circuit = fidelity_circuit.copy()
-        for i in range(3):
-            if state & (1 << i):
-                prepend_x(circuit, i)
-        run_main_loop([circuit], job_name="F3X_fidelity_computational_base_{:03b}".format(state))
-        circuits.append(circuit)
-
-    # run_main_loop(circuits, job_name="F3X_fidelity_computational_base_all")
-
-    # test_locally(circuits)
-
-    # test_locally_with_noise(circuits)
+n = 3
+circuit_name = "F3X.txt"
+job_base_name = "F3X_fidelity_computational_base"
+logical_to_physical = [0, 1, 2]
 
 
 def prepend_x(circuit, qubit):
     circuit.x(qubit)
     circuit.data = circuit.data[-1:] + circuit.data[:-1]
+
+
+def get_circuits():
+    fourier_circuit = QuantumCircuit.from_qasm_file('../circuits/{}'.format(circuit_name))
+    fourier_circuit.data = fourier_circuit.data[:-n]
+    inverted_fourier_circuit = fourier_circuit.inverse()
+    fourier_circuit.barrier()
+    fidelity_circuit = fourier_circuit.extend(inverted_fourier_circuit)
+    fidelity_circuit.measure(range(n), range(n))
+
+    circuits = []
+
+    for state in range(2 ** n):
+        circuit = fidelity_circuit.copy()
+        for i in range(n):
+            if state & (1 << i):
+                prepend_x(circuit, logical_to_physical[i])
+        circuits.append(circuit)
+
+    return circuits
+
+
+def main():
+    circuits = get_circuits()
+
+    # for state in range(2 ** n):
+    #     run_main_loop([circuits[state]], job_name="{}_{:03b}".format(job_base_name, state))
+
+    run_main_loop(circuits, job_name="{}_all".format(job_base_name))
+
+    # test_locally(circuits)
+
+    # test_locally_with_noise(circuits)
 
 
 if __name__ == '__main__':
