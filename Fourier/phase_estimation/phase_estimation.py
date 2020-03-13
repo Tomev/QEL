42c3f885b8109d1fbf0db2947a00962247b9be67
Wojciech Ciszewski <wojtrz13@gmail.com>
2020-03-13 21:37:25 +0100

    Added an option to specify the state to use for phase estimation

--- Fourier/phase_estimation/phase_estimation.py
+++ Fourier/phase_estimation/phase_estimation.py
@@ -6,10 +6,10 @@ from qiskit.extensions import U1Gate
 import numpy as np
 
 
-gate = U1Gate(np.pi)
+gate = U1Gate(2 * np.pi * (1 / 8 + 1 / 16 + 1 / 4 + 1 / 128))
 
 
-def phase_estimation_circuit(num_approximation_bits, gate, min_success_probability=None):
+def phase_estimation_circuit(num_approximation_bits, gate, initial_amplitudes=None, min_success_probability=None):
     if min_success_probability is not None:
         num_approximation_qubits = num_approximation_bits +\
                                    int(np.ceil(np.log2(2 + 1 / (2 * (1 - min_success_probability)))))
@@ -20,6 +20,10 @@ def phase_estimation_circuit(num_approximation_bits, gate, min_success_probabili
     qr = QuantumRegister(n)
     cr = ClassicalRegister(num_approximation_bits)
     qc = QuantumCircuit(qr, cr)
+
+    if initial_amplitudes is not None:
+        for qubits, amplitudes in initial_amplitudes:
+            qc.initialize(amplitudes, [idx + num_approximation_qubits for idx in qubits])
 
     for i in range(num_approximation_qubits):
         qc.h(qr[i])
@@ -40,7 +44,7 @@ def phase_estimation_circuit(num_approximation_bits, gate, min_success_probabili
 
 
 def main():
-    print(phase_estimation_circuit(3, gate, min_success_probability=0.5))
+    print(phase_estimation_circuit(7, gate, min_success_probability=0.5, initial_amplitudes=[([0], [0, 1])]))
 
 
 if __name__ == '__main__':
